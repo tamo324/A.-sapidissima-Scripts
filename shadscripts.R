e70fffd0a908d5de651918b5e47c7b7afe1c5e8e
ea227 <90070309+ea227@users.noreply.github.com>
2024-11-27 08:33:09 -0500

    Update shadscripts.R
    het plot

--- shadscripts.R
+++ shadscripts.R
@@ -105,6 +105,68 @@ ggplot(het2, aes(x = POS, y = roll_mean_nHet, color = CHR)) +
 hetwindow <- roll_mean(hetfilter, n = 5000, weights = NULL, by = 5000, fill = numeric(0),
           partial = FALSE, align = c("center"), normalize = TRUE,
           na.rm = FALSE)
+#heterozygosity
+het <- read_tsv("heteroz.tsv")
+View(het)
+head(het)
+colnames(het)
+hetfilter <- het %>%
+  filter(!str_starts(CHR, "NW")) %>%
+  arrange(CHR, POS) %>%
+  mutate(CHR = case_when(
+  CHR == "NC_014690.1" ~ "MT",
+  CHR == "NC_055957.1" ~ "1",
+  CHR == "NC_055958.1" ~ "2",
+  CHR == "NC_055959.1" ~ "3",
+  CHR == "NC_055960.1" ~ "4",
+  CHR == "NC_055961.1" ~ "5",
+  CHR == "NC_055962.1" ~ "6",
+  CHR == "NC_055963.1" ~ "7",
+  CHR == "NC_055964.1" ~ "8",
+  CHR == "NC_055965.1" ~ "9",
+  CHR == "NC_055966.1" ~ "10",
+  CHR == "NC_055967.1" ~ "11",
+  CHR == "NC_055968.1" ~ "12",
+  CHR == "NC_055969.1" ~ "13",
+  CHR == "NC_055970.1" ~ "14",
+  CHR == "NC_055971.1" ~ "15",
+  CHR == "NC_055972.1" ~ "16",
+  CHR == "NC_055973.1" ~ "17",
+  CHR == "NC_055974.1" ~ "18",
+  CHR == "NC_055975.1" ~ "19",
+  CHR == "NC_055976.1" ~ "20",
+  CHR == "NC_055977.1" ~ "21",
+  CHR == "NC_055978.1" ~ "22",
+  CHR == "NC_055979.1" ~ "23",
+  CHR == "NC_055980.1" ~ "24",
+  TRUE ~ CHR 
+))
+
+
+#filter out the reads not aligned
+#dplyr sliding window for this. plot het (y-axs) along position (x axis)
+
+#trying to make windows and apply (was told to use roll_mean_)
+#roll_mean(x, n = 1L, weights = NULL, by = 1L, fill = numeric(0),
+         # partial = FALSE, align = c("center", "left", "right"), normalize = TRUE,
+         # na.rm = FALSE)
+View(hetfilter)
+het2 <- hetfilter %>%
+  group_by(CHR) %>%
+  mutate(roll_mean_nHet = roll_mean(x = nHet, n = 3000, fill = NA, align = "center")) %>%
+  ungroup()
+ 
+
+# Scatterplot of rolling means - this is not working/ just looks horroble 
+hetplot <- ggplot(het2, aes(x = CHR, y = roll_mean_nHet, color = CHR)) +
+  geom_line() +
+  scale_color_viridis_d(name = "Chromosome") + 
+  labs(
+    x = "Genomic Position",
+    y = "Rolling Mean of Heterozygosity (nHet)",
+    title = "Scatterplot of Rolling Mean Heterozygosity"
+  ) + theme_gray()
+print(hetplot)
 
 
 
